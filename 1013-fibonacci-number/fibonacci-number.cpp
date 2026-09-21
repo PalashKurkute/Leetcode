1983984c91d94b1ecce8de626825346991823e96
Palash Kurkute <115580277+PalashKurkute@users.noreply.github.com>
2026-09-21 20:21:40 +0530

    Time: 2 ms (77.58%) | Memory: 7.7 MB (65.09%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a=0,b=1,sum=0;
+        if(n==1)
+        {
+            return b;
+        }
+        for(int i=2;i<=n;i++)
+        {
+            sum=a+b;
+            a=b;
+            b=sum;
+        }
+        return sum;
+        
+    }
+};
